256e666f56fe2d1946344886e8b01178f9e7d914
SalimKobi <salimkobi9@gmail.com>
2022-03-09 00:34:38 +0100

    first commit

---
+++ Downloads/prj-cmp-01-starting-setup/src/components/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <!-- <li v-for="store in storedResources :key="storedResources.id">{{title}}</li> -->
+    
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'You need to learn to google',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
